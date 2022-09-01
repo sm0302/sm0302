5d2bf71e70915cb7d6ed9646bf6ace7249a60036
sm0302 <99257756+sm0302@users.noreply.github.com>
2022-09-01 15:25:21 +0900

    Add files via upload

---
+++ 쇼핑몰 프로젝트/mapper(java)/AdminMapper.java
@@ -0,0 +1,18 @@
+package com.ksm.mapper;
+
+import java.util.List;
+
+import com.ksm.domain.BookVO;
+import com.ksm.domain.CateVO;
+import com.ksm.domain.Criteria;
+
+public interface AdminMapper {
+	/* 상품 등록 */
+	public void bookEnroll(BookVO book);
+	/* 카테고리 리스트 */
+	public List<CateVO> cateList();
+	/* 상품 리스트 */
+	public List<BookVO> goodsGetList(Criteria cri);
+	/* 상품 총 개수 */
+	public int goodsGetTotal(Criteria cri);
+}
---
+++ 쇼핑몰 프로젝트/mapper(java)/MemberMapper.java
@@ -0,0 +1,12 @@
+package com.ksm.mapper;
+
+import com.ksm.domain.MemberVO;
+
+public interface MemberMapper {
+	//회원가입
+	public void memberJoin(MemberVO member);
+	// 아이디 중복 검사
+	public int idCheck(String memberId);
+	/* 로그인 */
+    public MemberVO memberLogin(MemberVO member);
+}
